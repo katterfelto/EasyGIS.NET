a661a23de1dd0549d44ca9dd0165631beb8be74f
Dave Moor <katterfelto@outlook.com>
2026-07-17 10:22:23 +0100

    Added TableLayoutPanels to manage controls

--- EGIS.Controls/CsvToShapeFileControl.Designer.cs
+++ EGIS.Controls/CsvToShapeFileControl.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.cbXCoordField = new System.Windows.Forms.ComboBox();
-            this.cbYCoordField = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtSource = new System.Windows.Forms.TextBox();
             this.btnBrowseSource = new System.Windows.Forms.Button();
@@ -43,68 +41,64 @@
             this.sfdShapeFile = new System.Windows.Forms.SaveFileDialog();
             this.lblCrsId = new System.Windows.Forms.Label();
             this.btnSelectCRS = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.cbXCoordField = new System.Windows.Forms.ComboBox();
+            this.cbYCoordField = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 65);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(150, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "X-Coord Field";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(178, 65);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(159, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.Size = new System.Drawing.Size(150, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Y-Coord Field";
-            // 
-            // cbXCoordField
-            // 
-            this.cbXCoordField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbXCoordField.FormattingEnabled = true;
-            this.cbXCoordField.Location = new System.Drawing.Point(3, 81);
-            this.cbXCoordField.Name = "cbXCoordField";
-            this.cbXCoordField.Size = new System.Drawing.Size(160, 21);
-            this.cbXCoordField.TabIndex = 2;
-            this.cbXCoordField.SelectedIndexChanged += new System.EventHandler(this.cbXCoordField_SelectedIndexChanged);
-            // 
-            // cbYCoordField
-            // 
-            this.cbYCoordField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbYCoordField.FormattingEnabled = true;
-            this.cbYCoordField.Location = new System.Drawing.Point(178, 81);
-            this.cbYCoordField.Name = "cbYCoordField";
-            this.cbYCoordField.Size = new System.Drawing.Size(160, 21);
-            this.cbYCoordField.TabIndex = 3;
-            this.cbYCoordField.SelectedIndexChanged += new System.EventHandler(this.cbYCoordField_SelectedIndexChanged);
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 8);
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.Size = new System.Drawing.Size(60, 29);
             this.label3.TabIndex = 4;
             this.label3.Text = "Source File";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // txtSource
             // 
             this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtSource.Location = new System.Drawing.Point(69, 5);
+            this.txtSource.Location = new System.Drawing.Point(69, 3);
             this.txtSource.Name = "txtSource";
-            this.txtSource.Size = new System.Drawing.Size(190, 20);
+            this.txtSource.Size = new System.Drawing.Size(159, 20);
             this.txtSource.TabIndex = 5;
             // 
             // btnBrowseSource
             // 
             this.btnBrowseSource.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowseSource.Location = new System.Drawing.Point(265, 3);
+            this.btnBrowseSource.Location = new System.Drawing.Point(234, 3);
             this.btnBrowseSource.Name = "btnBrowseSource";
             this.btnBrowseSource.Size = new System.Drawing.Size(75, 23);
             this.btnBrowseSource.TabIndex = 6;
@@ -115,7 +109,7 @@
             // btnBrowseShapeFile
             // 
             this.btnBrowseShapeFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowseShapeFile.Location = new System.Drawing.Point(265, 32);
+            this.btnBrowseShapeFile.Location = new System.Drawing.Point(234, 32);
             this.btnBrowseShapeFile.Name = "btnBrowseShapeFile";
             this.btnBrowseShapeFile.Size = new System.Drawing.Size(75, 23);
             this.btnBrowseShapeFile.TabIndex = 9;
@@ -127,24 +121,26 @@
             // 
             this.txtDestination.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtDestination.Location = new System.Drawing.Point(69, 34);
+            this.txtDestination.Location = new System.Drawing.Point(69, 32);
             this.txtDestination.Name = "txtDestination";
-            this.txtDestination.Size = new System.Drawing.Size(190, 20);
+            this.txtDestination.Size = new System.Drawing.Size(159, 20);
             this.txtDestination.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 37);
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 29);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.Size = new System.Drawing.Size(60, 29);
             this.label4.TabIndex = 7;
             this.label4.Text = "Destination";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // btnConvert
             // 
             this.btnConvert.Enabled = false;
-            this.btnConvert.Location = new System.Drawing.Point(115, 137);
+            this.btnConvert.Location = new System.Drawing.Point(103, 155);
             this.btnConvert.Name = "btnConvert";
             this.btnConvert.Size = new System.Drawing.Size(112, 23);
             this.btnConvert.TabIndex = 10;
@@ -163,15 +159,17 @@
             // lblCrsId
             // 
             this.lblCrsId.AutoSize = true;
-            this.lblCrsId.Location = new System.Drawing.Point(4, 113);
+            this.lblCrsId.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCrsId.Location = new System.Drawing.Point(3, 0);
             this.lblCrsId.Name = "lblCrsId";
-            this.lblCrsId.Size = new System.Drawing.Size(72, 13);
+            this.lblCrsId.Size = new System.Drawing.Size(72, 29);
             this.lblCrsId.TabIndex = 12;
             this.lblCrsId.Text = "[CRS ID xxxx]";
+            this.lblCrsId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // btnSelectCRS
             // 
-            this.btnSelectCRS.Location = new System.Drawing.Point(82, 108);
+            this.btnSelectCRS.Location = new System.Drawing.Point(81, 3);
             this.btnSelectCRS.Name = "btnSelectCRS";
             this.btnSelectCRS.Size = new System.Drawing.Size(64, 23);
             this.btnSelectCRS.TabIndex = 13;
@@ -179,27 +177,132 @@
             this.btnSelectCRS.UseVisualStyleBackColor = true;
             this.btnSelectCRS.Click += new System.EventHandler(this.btnSelectCRS_Click);
             // 
+            // tableLayoutPanel1
+            //
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnConvert, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(318, 182);
+            this.tableLayoutPanel1.TabIndex = 14;
+            //
+            // tableLayoutPanel3
+            //
+            this.tableLayoutPanel3.AutoSize = true;
+            this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel3, 3);
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.cbXCoordField, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.cbYCoordField, 1, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 67);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(312, 47);
+            this.tableLayoutPanel3.TabIndex = 15;
+            //
+            // cbXCoordField
+            //
+            this.cbXCoordField.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbXCoordField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbXCoordField.FormattingEnabled = true;
+            this.cbXCoordField.Location = new System.Drawing.Point(3, 23);
+            this.cbXCoordField.Name = "cbXCoordField";
+            this.cbXCoordField.Size = new System.Drawing.Size(150, 21);
+            this.cbXCoordField.TabIndex = 2;
+            this.cbXCoordField.SelectedIndexChanged += new System.EventHandler(this.cbXCoordField_SelectedIndexChanged);
+            //
+            // cbYCoordField
+            //
+            this.cbYCoordField.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbYCoordField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbYCoordField.FormattingEnabled = true;
+            this.cbYCoordField.Location = new System.Drawing.Point(159, 23);
+            this.cbYCoordField.Name = "cbYCoordField";
+            this.cbYCoordField.Size = new System.Drawing.Size(150, 21);
+            this.cbYCoordField.TabIndex = 3;
+            this.cbYCoordField.SelectedIndexChanged += new System.EventHandler(this.cbYCoordField_SelectedIndexChanged);
+            //
+            // tableLayoutPanel2
+            //
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 3);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.txtSource, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtDestination, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnBrowseSource, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnBrowseShapeFile, 2, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(312, 58);
+            this.tableLayoutPanel2.TabIndex = 15;
+            //
+            // tableLayoutPanel4
+            //
+            this.tableLayoutPanel4.AutoSize = true;
+            this.tableLayoutPanel4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel4, 3);
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.lblCrsId, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.btnSelectCRS, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 120);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(312, 29);
+            this.tableLayoutPanel4.TabIndex = 15;
+            //
             // CsvToShapeFileControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btnSelectCRS);
-            this.Controls.Add(this.lblCrsId);
-            this.Controls.Add(this.btnConvert);
-            this.Controls.Add(this.btnBrowseShapeFile);
-            this.Controls.Add(this.txtDestination);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.btnBrowseSource);
-            this.Controls.Add(this.txtSource);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.cbYCoordField);
-            this.Controls.Add(this.cbXCoordField);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "CsvToShapeFileControl";
-            this.Size = new System.Drawing.Size(343, 165);
+            this.Size = new System.Drawing.Size(318, 182);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -207,8 +310,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cbXCoordField;
-        private System.Windows.Forms.ComboBox cbYCoordField;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtSource;
         private System.Windows.Forms.Button btnBrowseSource;
@@ -220,5 +321,11 @@
         private System.Windows.Forms.SaveFileDialog sfdShapeFile;
         private System.Windows.Forms.Label lblCrsId;
         private System.Windows.Forms.Button btnSelectCRS;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.ComboBox cbXCoordField;
+        private System.Windows.Forms.ComboBox cbYCoordField;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
     }
 }
